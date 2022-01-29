e9cb5dd5aff129a7635b2bfc178cbeb8be1f2ce0
Louis Beugnon <louis.beugnon@epitech.eu>
2022-01-29 19:15:48 +0100

    Create Response check bottom part

--- components/BottomPart/choices.js
+++ components/BottomPart/choices.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 import AnswerSelector from './answerSelector';
 
 export default function Choices() {
+  const [isResponseSelected, setIsResponseSelected] = useState(false);
+
+  const selectOnClick = () => { setIsResponseSelected(true) };
+
   return (
     <View style={styles.choicesContainer}>
       <Text style={styles.choiceTitleText}>SELECT A CHOICE</Text>
-      <AnswerSelector answer={{label: "Answer 1", order: "A"}}/>
-      <AnswerSelector answer={{label: "Answer 2", order: "B"}}/>
-      <AnswerSelector answer={{label: "Answer 3", order: "C"}}/>
-      <AnswerSelector answer={{label: "Answer 4", order: "D"}}/>
+      <AnswerSelector answer={{label: "Answer 1", order: "A", percentage: "55", isCorrect: true}} isResponse={isResponseSelected} onClick={selectOnClick} />
+      <AnswerSelector answer={{label: "Answer 2", order: "B", percentage: "17", isCorrect: false}} isResponse={isResponseSelected} onClick={selectOnClick}/>
+      <AnswerSelector answer={{label: "Answer 3", order: "C", percentage: "24", isCorrect: false}} isResponse={isResponseSelected} onClick={selectOnClick}/>
+      <AnswerSelector answer={{label: "Answer 4", order: "D", percentage: "4", isCorrect: false}} isResponse={isResponseSelected} onClick={selectOnClick}/>
     </View>
   );
 }
